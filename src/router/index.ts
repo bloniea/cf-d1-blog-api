f7c50e885f976dff364bca3dccec9c71c230077e
bloniea <bloniea@qq.com>
2024-03-16 16:18:40 +0800

    修改数据库

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,13 +17,14 @@ import { createRole, deleteRole, getRole, getRoles, updateRole } from "./roles"
 import { createUser, deleteUser, getUser, getUsers, updateUser } from "./users"
 import { Authorization } from "../utils/middleware"
 import { login, refreshToken } from "./login"
+import { sha256 } from "hono/utils/crypto"
 type Variables = {
   message: string
 }
 
 const app = new Hono<{ Variables: Variables }>()
 app.get("/", async (c: Context) => {
-  return c.json({ message: c.env.TEST })
+  return c.json({ message: "主页" })
 })
 const blogApi = app.basePath("/api/v1/blog/")
 blogApi.use("/*", Authorization)
